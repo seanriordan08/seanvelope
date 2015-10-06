b0235763b4253e1c2445bd61e5e8052746cfac2e
seanriordan08 <sean.riordan08@gmail.com>
2015-10-05 21:52:40 -0600

    Build out article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,30 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+
+    @article = Article.new(article_params)
+
+    respond_to do |format|
+      if @article.save
+        flash[:success] = "Save Success!"
+        format.html { redirect_to articles_path }
+      else
+        flash[:error] = "Save Failed!"
+        format.html { redirect_to articles_path }
+      end
+    end
+
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit!
+  end
 end
