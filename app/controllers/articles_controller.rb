cbe35cda946c0ccb90d7006ea27b87d2031a6d9f
seanriordan08 <sean.riordan08@gmail.com>
2015-10-06 10:02:38 -0600

    Fix edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
 
   end
@@ -22,6 +26,36 @@ class ArticlesController < ApplicationController
       end
     end
 
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+
+    respond_to do |format|
+      format.html { render "edit" }
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    respond_to do |format|
+      if @article.update_attributes!(article_params)
+        flash[:success] = "Success!"
+      else
+        flash[:error] = "Errors!"
+      end
+      format.html { redirect_to articles_path }
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    respond_to do |format|
+      format.html { redirect_to articles_path }
+    end
   end
 
   private
