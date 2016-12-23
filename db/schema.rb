d5ffb85c06060ea7bf91e76fe560ed0505171de1
seanriordan08 <sean.riordan08@gmail.com>
2016-12-22 22:05:20 -0700

    Add names, header styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223040533) do
+ActiveRecord::Schema.define(version: 20161223045544) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -51,6 +51,8 @@ ActiveRecord::Schema.define(version: 20161223040533) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "first_name",             limit: 255,              null: false
+    t.string   "last_name",              limit: 255,              null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
