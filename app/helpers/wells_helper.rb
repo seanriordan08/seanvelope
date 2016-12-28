6058c396609c7df3840b77856ddc462da52a2190
seanriordan08 <sean.riordan08@gmail.com>
2016-12-27 19:58:49 -0700

    Add well form fields

--- app/helpers/wells_helper.rb
+++ app/helpers/wells_helper.rb
@@ -1,2 +1,34 @@
 module WellsHelper
+
+  def get_customers
+    customer_ids = BasinMetrics::Customer.all.map(&:id)
+    customer_names = BasinMetrics::Customer.all.map(&:name)
+
+    build_customer_options(customer_ids, customer_names)
+  end
+
+  def get_customers
+    customer_ids = BasinMetrics::Customer.all.map(&:id)
+    customer_names = BasinMetrics::Customer.all.map(&:name)
+
+    build_options(customer_ids, customer_names)
+  end
+
+  def get_districts
+    district_ids = BasinMetrics::District.all.map(&:id)
+    district_names = BasinMetrics::District.all.map(&:name)
+
+    build_options(district_ids, district_names)
+  end
+
+
+  private
+
+  def build_options(ids, names)
+    options = []
+    ids.each_with_index do |c_id, index|
+      options << [names[index], c_id]
+    end
+    options
+  end
 end
