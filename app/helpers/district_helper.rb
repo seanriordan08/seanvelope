4b424aaa4541b36d7019c6b1de3c947dbe9a2bf5
seanriordan08 <sean.riordan08@gmail.com>
2017-01-03 23:29:18 -0700

    Fix districts chart

--- app/helpers/district_helper.rb
+++ app/helpers/district_helper.rb
@@ -1,10 +1,11 @@
 module DistrictHelper
 
-  def get_district_customer_counts
-    districts = BasinMetrics::District.all.order(:name)
-    districts.collect do |c|
-      BasinMetrics::Well.where(customer_id: c).size
-    end
+  def get_district_well_counts
+    x_label = 'District'
+    y_label = 'Wells'
+    district_well_count = BasinMetrics::District.connection.select_all("SELECT districts.name, COUNT(wells.name) as well_name FROM `districts` LEFT JOIN `wells` ON `wells`.`district_id` = `districts`.`id` GROUP BY `districts`.`name` ORDER BY `districts`.`name` ASC")
+    array = district_well_count.rows.collect{|row| row[1]}
+    {array: array, x_label: x_label, y_label: y_label}
   end
 
 end
