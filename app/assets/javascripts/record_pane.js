489ae28523d86b222071c6d143234881be19cd1b
seanriordan08 <sean.riordan08@gmail.com>
2017-01-08 15:42:02 -0700

    clean consoles

--- app/assets/javascripts/record_pane.js
+++ app/assets/javascripts/record_pane.js
@@ -19,3 +19,33 @@ function closeRecordPaneDirectly(el){
   });
   el.removeClass('showing');
 }
+
+function sendRecordUpdate(el, new_content){
+  var context = getRecordContext(el);
+  //var record = el.closest('.'+ context +'_record');
+  //var record_attr = el.closest('.record_attribute');
+  //var context_id = record.data(context + 'id');
+  //var param_key = record_attr.data("name");
+  //$.ajax({
+  //  method: 'PUT',
+  //  url: '/basin_metrics/' + context + 's/' + context_id,
+  //  data: param_key + '=' + new_content,
+  //  dataType: 'script'
+  //});
+  //cleanNulls(el, new_content);
+}
+
+function getRecordContext(el) {
+  var el_class = el.closest('tr').attr('class');
+  if (el_class.indexOf('well') >= 0){
+    return 'well';
+  } else if (el_class.indexOf('part') >= 0){
+    return 'part';
+  }
+}
+
+function setNullableRecordFieldColors() {
+  //$('.record_attribute.nullable div').each(function(index){
+  //  cleanNulls($(this), $(this).text());
+  //});
+}
