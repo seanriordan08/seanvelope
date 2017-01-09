b89f9fa3b64b8cec6a88739c711b7e2667af6798
seanriordan08 <sean.riordan08@gmail.com>
2017-01-08 22:02:59 -0700

    clean part graph

--- app/helpers/parts_helper.rb
+++ app/helpers/parts_helper.rb
@@ -29,22 +29,26 @@ module PartsHelper
     wells_build_options(well_ids, well_numbers)
   end
 
-  def get_monthly_plug_quantities
+  def get_monthly_plugs_sold
+    get_plug_quantities('date_sold')
+  end
+
+  private
+
+  def get_plug_quantities(target_attribute)
     current_year = Time.zone.now.strftime('%Y').to_i
-    well_x_label = 'Month'
-    well_y_label = 'Plugs Sold'
+    well_x_label = "Month of #{current_year}"
+    well_y_label = 'Quantity'
     quantity_by_month = []
     (1..12).each do |m|
       first_day = Date.civil(current_year, m, 1).to_datetime.utc
       last_day = Date.civil(current_year, m, -1).to_datetime.utc
-      plugs = BasinMetrics::Part.where(name: 'plug', date_sold: first_day..last_day)
+      plugs = BasinMetrics::Part.where(name: 'plug', "#{target_attribute}": first_day..last_day)
       quantity = plugs.collect(&:quantity)
       quantity_by_month << quantity.sum
     end
     {array: quantity_by_month, x_label: well_x_label, y_label: well_y_label}
   end
-
-  private
 
   def wells_build_options(ids, numbers)
     options = [['well number', '']]
