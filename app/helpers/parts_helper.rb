83774d56c67a46e8fa40f7bbba83fdcb0e88cbf0
seanriordan08 <sean.riordan08@gmail.com>
2017-01-10 15:27:00 -0700

    stuff

--- app/helpers/parts_helper.rb
+++ app/helpers/parts_helper.rb
@@ -1,22 +1,11 @@
 module PartsHelper
 
   def get_part_headings
-    base_headings = BasinMetrics::Part.first
-    base_headings = base_headings.attribute_names
-    omission = %w(id created_at updated_at)
-
-    base_headings.insert(1, 'customer')
-    omission.each do |omit_value|
-      base_headings.delete_at(base_headings.index(omit_value))
-    end
-
-    base_headings
+    %W(customer name type number size quantity order_number revenue date_sold well_id)
   end
 
   def get_part_customer_name(part_id)
-    well_id = BasinMetrics::Part.where(id: part_id).first.well_id
-    customer_id = BasinMetrics::Well.where(id: well_id).first.customer_id
-    BasinMetrics::Customer.find(customer_id).name.titleize
+    current_user.company.parts.where(id: part_id).first.well.customer.name.titleize
   end
 
   def get_well(id)
@@ -24,8 +13,8 @@ module PartsHelper
   end
 
   def get_parts_options
-    well_ids = BasinMetrics::Well.all.map(&:id)
-    well_numbers = BasinMetrics::Well.all.map(&:number)
+    well_ids = current_user.company.wells.map(&:id)
+    well_numbers = current_user.company.wells.map(&:number)
     wells_build_options(well_ids, well_numbers)
   end
 
