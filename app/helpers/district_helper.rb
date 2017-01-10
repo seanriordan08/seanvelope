ada99008bfcd491c81d2ce2de1212ad6d4d23230
seanriordan08 <sean.riordan08@gmail.com>
2017-01-10 14:05:24 -0700

    clean district headings

--- app/helpers/district_helper.rb
+++ app/helpers/district_helper.rb
@@ -6,16 +6,7 @@ module DistrictHelper
   COLOR_ABSENT_HOVER = '#2D3D47'
 
   def get_district_headings
-    base_headings = BasinMetrics::District.first
-    base_headings = base_headings.attribute_names
-    omission = %w(id)
-
-    omission.each do |omit_value|
-      base_headings.delete_at(base_headings.index(omit_value))
-    end
-
-    base_headings.unshift('')
-    base_headings
+    %W(#{''} name created_at updated_at)
   end
 
   def get_district_well_counts
