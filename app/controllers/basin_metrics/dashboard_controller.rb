3fb5f6fab4036467c440f8b10379322cbf50c9f9
seanriordan08 <sean.riordan08@gmail.com>
2017-01-11 12:25:01 -0700

    pull bullet temp

--- app/controllers/basin_metrics/dashboard_controller.rb
+++ app/controllers/basin_metrics/dashboard_controller.rb
@@ -3,8 +3,8 @@ class BasinMetrics::DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @customers = BasinMetrics::Customer.all
-    @districts = BasinMetrics::District.all
+    @customers = current_user.company.customers
+    @districts = current_user.company.districts
   end
 
 end
