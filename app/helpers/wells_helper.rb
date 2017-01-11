331c63a851f5a5e0341fde1c3167e8232e6c58f9
seanriordan08 <sean.riordan08@gmail.com>
2017-01-11 02:08:17 -0700

    add well mixed chart type

--- app/helpers/wells_helper.rb
+++ app/helpers/wells_helper.rb
@@ -29,17 +29,17 @@ module WellsHelper
   end
 
   def get_background_colors
-    customer_count = BasinMetrics::Customer.all.order(:name).collect(&:name).size
+    # customer_count = current_user.company.wells.order(:name).collect(&:name).size
     background_colors = []
-    customer_count.times {background_colors << 'rgba(75, 192, 192, 0.2)'}
+    24.times {background_colors << 'rgba(75, 192, 192, 0.2)'}
     background_colors
     # [ 'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)']
   end
 
   def get_border_colors
-    customer_count = BasinMetrics::Customer.all.order(:name).collect(&:name).size
+    # customer_count = current_user.company.wells.order(:name).collect(&:name).size
     border_colors = []
-    customer_count.times {border_colors << 'rgba(75, 192, 192, 1)'}
+    24.times {border_colors << 'rgba(75, 192, 192, 1)'}
     border_colors
     # ['rgba(255,99,132,1)', 'rgba(54, 162, 235, 1)', 'rgba(255, 206, 86, 1)', 'rgba(75, 192, 192, 1)', 'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)']
   end
